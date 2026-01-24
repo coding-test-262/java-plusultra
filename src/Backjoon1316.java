9d25c0346a4306061df5140466b9e357b8169722
PlusUltraCode <kkd06144@naver.com>
2026-01-24 13:21:24 +0900

    first commit

--- src/Backjoon1316.java
+++ src/Backjoon1316.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Backjoon1316 {
+    static Scanner sc = new Scanner(System.in);
+    static HashMap<Character, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        int N = sc.nextInt();
+        for(int i=0;i<N;i++){
+            String str = sc.next();
+
+        }
+    }
+
+    private static void enroll(String str) {
+        
+    }
+
 }
