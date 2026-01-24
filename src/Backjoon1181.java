fdfba4f681e27e1cf35b38a52b23151148a799d6
PlusUltraCode <kkd06144@naver.com>
2026-01-24 14:34:16 +0900

    백준 1181 java 버전

---
+++ src/Backjoon1181.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Backjoon1181 {
+
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static ArrayList<String> strs = new ArrayList<>();
+    static Set<String> mySet = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bf.readLine());
+
+        for(int i=0;i<N;i++){
+            String str = bf.readLine();
+            mySet.add(str);
+        }
+        strs = new ArrayList<>(mySet);
+        Collections.sort(strs, (a,b)->{
+            if(a.length()!=b.length()){
+                return a.length() - b.length();
+            }
+            return a.compareTo(b);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(String str: strs){
+            sb.append(str).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
