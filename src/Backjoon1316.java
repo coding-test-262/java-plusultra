236418baa53a319c429861b5be250cd7871986f2
PlusUltraCode <kkd06144@naver.com>
2026-01-24 13:39:07 +0900

    백준 1316 java 버전

--- src/Backjoon1316.java
+++ src/Backjoon1316.java
@@ -7,14 +7,34 @@ public class Backjoon1316 {
 
     public static void main(String[] args) {
         int N = sc.nextInt();
+
+        int count = 0;
         for(int i=0;i<N;i++){
             String str = sc.next();
-
+            if(enroll(str)) count++;
+            map.clear();
         }
+
+        System.out.println(count);
+
+
     }
 
-    private static void enroll(String str) {
-        
+    private static boolean enroll(String str) {
+
+        boolean newWord = false;
+        char prevCh = '#';
+        for (int i = 0; i < str.length(); i++) {
+            char nowCh = str.charAt(i);
+            if(map.containsKey(nowCh)){
+            if(map.get(nowCh)==1 && prevCh!=nowCh) {
+                return false;
+            }
+            }
+            map.put(nowCh,1);
+            prevCh = nowCh;
+        }
+        return true;
     }
 
 }
