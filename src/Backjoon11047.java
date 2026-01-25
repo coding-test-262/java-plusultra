03412b2a6ceb1deb944a413d29ebfa639314a88b
PlusUltraCode <kkd06144@naver.com>
2026-01-25 09:01:22 +0900

    백준 11047 java 버전

---
+++ src/Backjoon11047.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Backjoon11047 {
+    static Scanner sc = new Scanner(System.in);
+    static int N,K;
+    static ArrayList<Integer> arrs = new ArrayList<>();
+
+    private static void input(){
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        for(int i=0;i<N;i++) {
+            int num = sc.nextInt();
+            arrs.add(num);
+        }
+    }
+
+    private static void gameStart() {
+        arrs.sort((a,b)->{
+            return -1*(a-b);
+        });
+
+        int resultCount = 0;
+
+        for(int i=0;i<N;i++){
+            int money = arrs.get(i);
+            resultCount += K/money;
+            K%=money;
+            if(K==0)break;
+        }
+
+        System.out.println(resultCount);
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        gameStart();
+
+    }
+}
