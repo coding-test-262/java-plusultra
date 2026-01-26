8b25f0db1e7ed0d13c90d1a52787c8dc0c91d85d
PlusUltraCode <kkd06144@naver.com>
2026-01-26 10:48:23 +0900

    백준 1929 java 버전

---
+++ src/Backjoon1929.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Backjoon1929 {
+    static int MAXSIZE = 1000000;
+    static Scanner sc = new Scanner(System.in);
+    static int N,M;
+    static boolean[] isPrime = new boolean[MAXSIZE+1];
+
+
+    private static void checkPrime(){
+        for(int i=2;i<=MAXSIZE;i++){
+            isPrime[i] = true;
+        }
+
+        for(int i=2;i*i<=MAXSIZE;i++){
+            if(isPrime[i]){
+                for(int k=i*i; k<=MAXSIZE;k+=i){
+                    isPrime[k] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        N = sc.nextInt();
+        M = sc.nextInt();
+        checkPrime();
+
+        for(int i=N;i<=M;i++){
+            if(isPrime[i]){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
