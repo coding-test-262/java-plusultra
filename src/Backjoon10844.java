0f9ee25248737e0d31696647d32b1bc186c68ea3
PlusUltraCode <kkd06144@naver.com>
2026-01-28 09:46:26 +0900

    백준 10844 java 버전

---
+++ src/Backjoon10844.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Backjoon10844 {
+    static int N;
+    static int [][]arr  = new int[101][10];
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        N = sc.nextInt();
+
+        for(int i=1;i<=9;i++){
+            arr[1][i] = 1;
+        }
+
+        for(int i=2;i<=N;i++){
+
+            for(int k=0;k<=9;k++){
+                if(k==0){
+                    arr[i][k] = arr[i-1][k+1];
+                    continue;
+                }
+                if(k==9){
+                    arr[i][k] = arr[i-1][k-1];
+                    continue;
+                }
+
+                arr[i][k] = (arr[i-1][k-1]+arr[i-1][k+1])%1000000000;
+            }
+        }
+
+        int resultSum = 0;
+        for(int i=0;i<=9;i++){
+            resultSum += arr[N][i];
+            resultSum%=1000000000;
+        }
+
+        System.out.println(resultSum);
+    }
+}
